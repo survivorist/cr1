b561c8cf8e2b7e16d25756d1e8a0f19d49b391c8
Tom Oakes <toaksie@gmail.com>
2012-06-24 17:53:42 +0200

    Stopped viewing of other users dashboards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-    
+    rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
  
   def after_sign_in_path_for(resource)
         @user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	rolify
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -9,6 +10,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
   								:first_name, :last_name
   # attr_accessible :title, :body
+  def role?(role)
+      return !!self.roles.find_by_name(role.to_s.camelize)
+  end
   
   has_many :items
 end
